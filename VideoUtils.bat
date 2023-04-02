a8b9b06dee53b9b50da28cad8c16d9abcca0a3b1
BladeSkilled <109127903+bladeskilled@users.noreply.github.com>
2023-04-02 13:42:35 -0700

    Update VideoUtils.bat

--- VideoUtils.bat
+++ VideoUtils.bat
@@ -4,13 +4,13 @@ title VideoUtils
 echo VideoUtils
 set /p input= type 1 to compress, press 2 to upscale, press 3 for both and press 4 to exit: 
 if "%input%"=="1" (
-    ffmpeg.exe -i %1 -c:v libx264 -vf scale=1920:1080:flags=neighbor -preset fast -rc constqp -qp 18 -c:a copy -r 60 "%~dpn1 - Compressed.mp4"
+    ffmpeg.exe -i %1 -c:v libx264 -vf scale=1920:1080:flags=neighbor -preset medium -rc constqp -crf 18 -c:a copy "%~dpn1 - Compressed.mp4"
 )
 if "%input%"=="2" (
-    ffmpeg.exe -i %1 -c:v h264_nvenc -vf scale=3840:2160:flags=neighbor -preset medium -rc constqp -qp 18 -c:a copy -r 60 "%~dpn1 - 4K.mp4"
+    ffmpeg.exe -i %1 -c:v h264_nvenc -vf scale=3840:2160:flags=neighbor -preset medium -rc constqp -crf 18 -c:a copy "%~dpn1 - 4K.mp4"
 )
 if "%input%"=="3" (
-    ffmpeg.exe -i %1 -c:v libx264 -vf scale=3840:2160:flags=neighbor -preset fast -rc constqp -qp 18 -c:a copy -r 60 "%~dpn1 - 4K - Compressed.mp4"
+    ffmpeg.exe -i %1 -c:v libx264 -vf scale=3840:2160:flags=neighbor -preset medium -rc constqp -crf 18 -c:a copy "%~dpn1 - 4K - Compressed.mp4"
 )
 if "%input%"=="4" (
     exit
