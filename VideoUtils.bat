d3f8fd3f0140f6e0db24e1bf389b0592973da70f
BladeSkilled <109127903+bladeskilled@users.noreply.github.com>
2023-04-03 15:33:35 -0700

    Update VideoUtils.bat

--- VideoUtils.bat
+++ VideoUtils.bat
@@ -9,13 +9,13 @@ title VideoUtils
 echo VideoUtils
 set /p input= type 1 to compress, press 2 to upscale, press 3 for both and press 4 to exit: 
 if "%input%"=="1" (
-C:\ffmpeg\bin\ffmpeg.exe -i %1 -c:v libx264 -vf scale=-2:1080:flags=bicubic -preset medium -crf 22 -c:a copy "%~dpn1 - Compressed.mp4"
+ffmpeg -i %1 -c:v libx264 -vf scale=-2:1080:flags=bicubic -preset medium -crf 22 -c:a copy "%~dpn1 - Compressed.mp4"
 )
 if "%input%"=="2" (
-C:\ffmpeg\bin\ffmpeg.exe -i %1 -c:v h264_nvenc -vf scale=-2:2160:flags=bicubic -preset medium -qp 18 -c:a copy "%~dpn1 - 4K.mp4"
+ffmpeg -i %1 -c:v h264_nvenc -vf scale=-2:2160:flags=bicubic -preset medium -qp 18 -c:a copy "%~dpn1 - 4K.mp4"
 )
 if "%input%"=="3" (
-C:\ffmpeg\bin\ffmpeg.exe -i %1 -c:v libx264 -vf scale=-2:2160:flags=bicubic -preset medium -crf 22 -c:a copy "%~dpn1 - 4K - Compressed.mp4"
+ffmpeg -i %1 -c:v libx264 -vf scale=-2:2160:flags=bicubic -preset medium -crf 22 -c:a copy "%~dpn1 - 4K - Compressed.mp4"
 )
 if "%input%"=="4" (
     exit
